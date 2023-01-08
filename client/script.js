d89e22894042bedc24810775317fa118e11a152c
NetworkFlux <75378448+NetworkFlux@users.noreply.github.com>
2023-01-08 13:16:07 +0100

    update to live

--- client/script.js
+++ client/script.js
@@ -85,7 +85,7 @@ const handleSubmit = async (e) => {
   Loader(messageDiv);
 
   // Fetch data from the server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codebot-nzk8.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
